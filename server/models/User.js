282ecb6a263c08f05d8965ec680d0285586dba75
Vinay <vinayreddaboina3574@gmail.com>
2026-02-17 22:40:34 +0530

    Created User Model Schema

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    skillsOffered: {
+      type: [String],
+      default: [],
+    },
+
+    skillsWanted: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
+
